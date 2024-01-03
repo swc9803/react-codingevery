259eb5291f27dc480e40cdda5eaa87efc4cd7b8e
swc9803 <swc9803@gmail.com>
2024-01-03 20:02:03 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.css'
 import Hello from './components/Hello.js'
-import Welcome from './components/Welcome.js'
 
 function App () {
   return (
     <div className='App'>
       <Hello />
-      <Welcome />
     </div>
   )
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import World from './World.js'
+import { React, useState } from 'react'
 import styles from './Hello.module.css'
 
 export default function Hello () {
+  const [name, setName] = useState('Mike')
+
+  const changeName = () => {
+    const newName = name === 'Mike' ? 'Jane' : 'Mike'
+    setName(newName)
+  }
+
   return (
-    <>
-      <p>hello components</p>
-      <World/>
-      <div className={styles.box}>hello</div>
-    </>
+    <div className={styles.wrapper}>
+      <h1>hello components</h1>
+      <p>{name}</p>
+      <button onClick={changeName}>Change name</button>
+    </div>
   )
 }
