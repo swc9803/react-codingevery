ed2d887c5bb74d07a85186de491356bccbe0d964
swc9803 <swc9803@gmail.com>
2024-01-03 19:42:48 +0900

    css 모듈 추가

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import World from './World.js'
+import styles from './Hello.module.css'
 
 export default function Hello () {
   return (
     <>
       <p>hello components</p>
       <World/>
+      <div className={styles.box}>hello</div>
     </>
   )
 }
