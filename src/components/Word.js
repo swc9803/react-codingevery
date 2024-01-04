f18c914c8cda6ac29ad05e94fe9ba7cd2ee05d9b
swc9803 <swc9803@gmail.com>
2024-01-04 14:37:25 +0900

    json delete

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,6 +1,7 @@
 import { React, useState } from 'react'
 
-const Header = ({ word }) => {
+const Header = (props) => {
+  const [word, setWord] = useState(props.word)
   const [isShow, setIsShow] = useState(false)
   const [isDone, setIsDone] = useState(word.isDone)
 
@@ -26,6 +27,22 @@ const Header = ({ word }) => {
       })
   }
 
+  const del = () => {
+    if (window.confirm('삭제 하시겠습니까?')) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: 'DELETE'
+      }).then(res => {
+        if (res.ok) {
+          setWord({ id: 0 })
+        }
+      })
+    }
+  }
+
+  if (word.id === 0) {
+    return null
+  }
+
   return (
     <tr className={isDone ? 'off' : ''}>
         <td>
@@ -35,7 +52,7 @@ const Header = ({ word }) => {
         <td>{isShow && word.kor}</td>
         <td>
             <button onClick={toggleShow}>뜻 {isShow ? '숨기기' : '보기'}</button>
-            <button className='btn_del'>삭제</button>
+            <button onClick={del} className='btn_del'>삭제</button>
         </td>
     </tr>
   )
