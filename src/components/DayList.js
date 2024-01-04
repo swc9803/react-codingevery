5464c2f0a7ca8bdbda2f1dce387540fc10415f9c
swc9803 <swc9803@gmail.com>
2024-01-04 14:01:13 +0900

    useEffect, fetch

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import dummy from '../db/data.json'
 
 const DayList = () => {
+  const [days, setDays] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/days')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setDays(data)
+      })
+  }, [])
+
   return (
     <>
         <ul className='list_day'>
-            {dummy.days.map(day => (
+            {days.map(day => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>Day {day.day}</Link>
                 </li>
